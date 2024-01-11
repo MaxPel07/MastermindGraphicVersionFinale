de554186d6d2dda71b2ec364fe565aa7856ae2da
Maxime Pelloquin <pt11pfm@eduvaud.ch>
2024-01-11 15:25:15 +0100

    Version Finale du Mastermind

---
+++ MastermindNormal.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Mastermind_Graphique
+{
+    partial class MastermindNormal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MastermindNormal));
+            this.btnPurple = new System.Windows.Forms.Button();
+            this.btnOrange = new System.Windows.Forms.Button();
+            this.btnMagenta = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.panelEssaie = new System.Windows.Forms.Panel();
+            this.panelIndice = new System.Windows.Forms.Panel();
+            this.chkSolution = new System.Windows.Forms.CheckBox();
+            this.panelSolution = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnPurple
+            // 
+            this.btnPurple.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnPurple.Location = new System.Drawing.Point(339, 534);
+            this.btnPurple.Name = "btnPurple";
+            this.btnPurple.Size = new System.Drawing.Size(75, 30);
+            this.btnPurple.TabIndex = 0;
+            this.btnPurple.UseVisualStyleBackColor = false;
+            this.btnPurple.Click += new System.EventHandler(this.ColorBouton_Click);
+            // 
+            // btnOrange
+            // 
+            this.btnOrange.BackColor = System.Drawing.Color.Orange;
+            this.btnOrange.Location = new System.Drawing.Point(339, 498);
+            this.btnOrange.Name = "btnOrange";
+            this.btnOrange.Size = new System.Drawing.Size(75, 30);
+            this.btnOrange.TabIndex = 1;
+            this.btnOrange.UseVisualStyleBackColor = false;
+            this.btnOrange.Click += new System.EventHandler(this.ColorBouton_Click);
+            // 
+            // btnMagenta
+            // 
+            this.btnMagenta.BackColor = System.Drawing.Color.Magenta;
+            this.btnMagenta.Location = new System.Drawing.Point(339, 462);
+            this.btnMagenta.Name = "btnMagenta";
+            this.btnMagenta.Size = new System.Drawing.Size(75, 30);
+            this.btnMagenta.TabIndex = 2;
+            this.btnMagenta.UseVisualStyleBackColor = false;
+            this.btnMagenta.Click += new System.EventHandler(this.ColorBouton_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.Location = new System.Drawing.Point(339, 426);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(75, 30);
+            this.btnYellow.TabIndex = 3;
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.ColorBouton_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnBlue.Location = new System.Drawing.Point(339, 390);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(75, 30);
+            this.btnBlue.TabIndex = 4;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.ColorBouton_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnGreen.Location = new System.Drawing.Point(339, 354);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(75, 30);
+            this.btnGreen.TabIndex = 5;
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.ColorBouton_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(339, 318);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(75, 30);
+            this.btnRed.TabIndex = 6;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.ColorBouton_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(339, 30);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 34);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(339, 82);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 34);
+            this.btnNewGame.TabIndex = 8;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(28, 512);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(54, 52);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(138, 512);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(91, 52);
+            this.btnConfirm.TabIndex = 10;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // panelEssaie
+            // 
+            this.panelEssaie.Location = new System.Drawing.Point(28, 30);
+            this.panelEssaie.Name = "panelEssaie";
+            this.panelEssaie.Size = new System.Drawing.Size(201, 462);
+            this.panelEssaie.TabIndex = 13;
+            // 
+            // panelIndice
+            // 
+            this.panelIndice.Location = new System.Drawing.Point(235, 30);
+            this.panelIndice.Name = "panelIndice";
+            this.panelIndice.Size = new System.Drawing.Size(76, 462);
+            this.panelIndice.TabIndex = 0;
+            // 
+            // chkSolution
+            // 
+            this.chkSolution.AutoSize = true;
+            this.chkSolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.chkSolution.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.chkSolution.Location = new System.Drawing.Point(339, 134);
+            this.chkSolution.Name = "chkSolution";
+            this.chkSolution.Size = new System.Drawing.Size(74, 20);
+            this.chkSolution.TabIndex = 14;
+            this.chkSolution.Text = "Solution";
+            this.chkSolution.UseVisualStyleBackColor = true;
+            this.chkSolution.CheckedChanged += new System.EventHandler(this.chkSolution_CheckedChanged);
+            // 
+            // panelSolution
+            // 
+            this.panelSolution.Location = new System.Drawing.Point(336, 156);
+            this.panelSolution.Name = "panelSolution";
+            this.panelSolution.Size = new System.Drawing.Size(78, 26);
+            this.panelSolution.TabIndex = 15;
+            // 
+            // MastermindNormal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(443, 583);
+            this.Controls.Add(this.panelSolution);
+            this.Controls.Add(this.chkSolution);
+            this.Controls.Add(this.panelIndice);
+            this.Controls.Add(this.panelEssaie);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnMagenta);
+            this.Controls.Add(this.btnOrange);
+            this.Controls.Add(this.btnPurple);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MastermindNormal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MasterMind - Normal";
+            this.Load += new System.EventHandler(this.MastermindNormal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPurple;
+        private System.Windows.Forms.Button btnOrange;
+        private System.Windows.Forms.Button btnMagenta;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Panel panelEssaie;
+        private System.Windows.Forms.Panel panelIndice;
+        private System.Windows.Forms.CheckBox chkSolution;
+        private System.Windows.Forms.Panel panelSolution;
+    }
+}
+
---
+++ MastermindNormal.cs
@@ -0,0 +1,324 @@
+﻿/// ETML
+/// Auteur      : Maxime Pelloquin
+/// Date        : 16.11.2023
+/// Descritpion :
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mastermind_Graphique
+{
+    public partial class MastermindNormal : System.Windows.Forms.Form
+    {
+        Label[,] tableauLabel;
+        Label[,] copieTableauLabel = new Label[LIGNES,COLONES];
+        Label[,] choixUtilisateur = new Label[LIGNES,COLONES];
+        Label[,] tableauLabelIndice;
+        Label[] tableauLabelSolution;
+        Color[] goal = new Color[nbCouleurMaxParEssaie];
+        Color[] copieGoal = new Color[nbCouleurMaxParEssaie];
+
+        const int COLONES = 4;
+        const int LIGNES = 10;
+        int conteurCouleur = 0;
+        int conteurEssaie = 0;
+        int positionY = 0;
+        int bonnePosition = 0;
+        int mauvaisePosition = 0;
+
+        public const int nbCouleurMaxParEssaie = 4;
+        public MastermindNormal()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Permet de Fermer le programme
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Ferme le programme
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Ferme le jeu et ouvre le menu du jeu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {   
+            // ouvre une nouvelle fois l'application
+            System.Diagnostics.Process.Start(Application.ExecutablePath);
+
+            // ferme l'ancienne application
+            this.Close();
+        }
+
+        /// <summary>
+        /// Ajoute les couleur des boutons cliqué dans le tableau de label des essaies du joueur
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ColorBouton_Click(object sender, EventArgs e)
+        {
+            Button colorBouton = (Button)sender;
+            tableauLabel[conteurCouleur, positionY].BackColor = colorBouton.BackColor;
+
+            
+            conteurCouleur++;
+            if (conteurCouleur == 4)
+            {
+                btnRed.Enabled = false;
+                btnGreen.Enabled = false;
+                btnBlue.Enabled = false;
+                btnYellow.Enabled = false;
+                btnMagenta.Enabled = false;
+                btnOrange.Enabled = false;
+                btnPurple.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Appelle les méthode permettant de : afficher le tableau de label pour les essais du joueur,
+        /// afficher le tableau de label des indices et la création aléatoire de la solution.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MastermindNormal_Load(object sender, EventArgs e)
+        {
+            CreationLabelEssaie(panelEssaie);
+            CreationLabelIndice(panelIndice);
+            NouvelleCombinaisonAléatoire().CopyTo(copieGoal,0);
+        }
+
+        private void tableLayoutPanel_Paint(object sender, PaintEventArgs e)
+        {   
+            // création d'un tableau de label
+            tableauLabel = new Label[10, 4];
+
+            for (int ligne = 0; ligne < 10; ligne++)
+            {
+                for (int colonne = 0; colonne < 4; colonne++)
+                {
+                    // création d'un label
+                    Label newLabel = new Label();
+
+                    // placement du label dans le tableau
+                    tableauLabel[ligne, colonne] = newLabel;
+
+                    // paramètre label
+                    newLabel.BackColor = Color.Yellow;
+                    newLabel.Size = new Size(20, 20);
+                    newLabel.Location = new Point(20*ligne, 20*colonne);
+
+                    // Ajout du label au Panel
+                    panelEssaie.Controls.Add(newLabel);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Affichage dynamique du tableau de label que remplira le joueur pour ses essaies.
+        /// </summary>
+        /// <param name="panel"></param>
+        private void CreationLabelEssaie(Panel panel)
+        {
+            // création d'un tableau de label
+            tableauLabel = new Label[4, 10];
+
+            for (int colonne = 0; colonne < 4; colonne++)
+            {
+                for (int ligne = 0; ligne < 10; ligne++)
+                {
+                    // création d'un label
+                    Label newLabel = new Label();
+
+                    // placement du label dans le tableau
+                    tableauLabel[colonne, ligne] = newLabel;
+
+                    // paramètre label
+                    newLabel.BackColor = Color.Gray;
+                    newLabel.Size = new Size(36, 36);
+                    newLabel.Location = new Point(45 * colonne + 2, 45 * ligne +2);
+
+                    // Ajout du label au Panel
+                    panel.Controls.Add(newLabel);
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Affichage dynamique du tableau de label des Indices.
+        /// </summary>
+        /// <param name="panel"></param>
+        private void CreationLabelIndice(Panel panel)
+        {
+            // création d'un tableau de label
+            tableauLabelIndice = new Label[4, 10];
+
+            for (int colonne = 0; colonne < 4; colonne++)
+            {
+                for (int ligne = 0; ligne < 10; ligne++)
+                {
+                    // création d'un label
+                    Label newLabel = new Label();
+
+                    // placement du label dans le tableau
+                    tableauLabelIndice[colonne, ligne] = newLabel;
+
+
+                    // paramètre label
+                    newLabel.BackColor = Color.Gray;
+                    newLabel.Size = new Size(10, 10);
+                    newLabel.Location = new Point(20 * colonne + 2, 45 * ligne + 15);
+
+                    // Ajout du label au Panel
+                    panel.Controls.Add(newLabel);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Création aléatoire de la solution. 
+        /// </summary>
+        /// <returns></returns>
+        private Color[] NouvelleCombinaisonAléatoire()
+        {
+            
+            Random random = new Random();
+
+            // Génération d'une combinaison aléatoire de 4 couleurs
+            for (int i = 0; i < nbCouleurMaxParEssaie; i++)
+            {
+                // nombre tiré entre 0 et 6 compris
+                int valeurDeCouleur = random.Next(6);
+                Color color;
+
+                // Conversion de la valeur aléatoire en couleur
+                switch (valeurDeCouleur)
+                {
+                    case 0:
+                        color = Color.Red;
+                        break;
+                    case 1:
+                        color = Color.Chartreuse;
+                        break;
+                    case 2:
+                        color = Color.DarkTurquoise;
+                        break;
+                    case 3:
+                        color = Color.Yellow;
+                        break;
+                    case 4:
+                        color = Color.Magenta;
+                        break;
+                    case 5:
+                        color = Color.Orange;
+                        break;
+                    default:
+                        color = Color.MediumPurple;
+                        break;
+                }
+
+                goal[i] = color;
+            }
+            // retourne le tableau de couleur "goal"
+            return goal;
+        }
+
+        /// <summary>
+        /// Affiche dans un tableau de label la solution
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+       private void chkSolution_CheckedChanged(object sender, EventArgs e)
+        {
+            tableauLabelSolution = new Label[4];
+
+            for (int ligne = 0; ligne < 4; ligne++)
+            {
+                // création d'un label
+                Label newLabel = new Label();
+
+                // placement du label dans le tableau
+                tableauLabelSolution[ligne] = newLabel;
+
+                // paramètre label
+                newLabel.BackColor = goal[ligne];
+                newLabel.Size = new Size(15, 15);
+                newLabel.Location = new Point(20 * ligne + 0);
+
+                // Ajout du label au Panel
+                panelSolution.Controls.Add(newLabel);
+            }
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            conteurCouleur = 0;
+
+            for (int i = 0; i < COLONES; i++)
+            {
+                if (goal[i] == tableauLabel[i,positionY].BackColor)
+                {
+                    tableauLabelIndice[bonnePosition, positionY].BackColor = Color.White;
+                    bonnePosition++;
+
+                    if (bonnePosition == 4)
+                    {
+                        MessageBox.Show("Bravo ! Vous avez gagné");
+
+                        btnConfirm.Enabled = false;
+                        btnClear.Enabled = false;
+
+                        btnRed.Enabled = false;
+                        btnGreen.Enabled = false;
+                        btnBlue.Enabled = false;
+                        btnYellow.Enabled = false;
+                        btnMagenta.Enabled = false;
+                        btnOrange.Enabled = false;
+                        btnPurple.Enabled = false;
+                    }
+                }
+
+                if (bonnePosition!=4)
+                {
+                    btnConfirm.Enabled = true;
+                    btnClear.Enabled = true;
+
+                    btnRed.Enabled = true;
+                    btnGreen.Enabled = true;
+                    btnBlue.Enabled = true;
+                    btnYellow.Enabled = true;
+                    btnMagenta.Enabled = true;
+                    btnOrange.Enabled = true;
+                    btnPurple.Enabled = true;
+                }
+                mauvaisePosition = bonnePosition;
+            }
+
+
+            positionY++;
+            conteurCouleur = 0;
+            bonnePosition = 0;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            conteurCouleur--;
+            tableauLabel[conteurCouleur, positionY].BackColor = Color.Gray;
+            // changer les couleurs des labels de l'essaie en cours en gris
+            
+        }
+    }
+}
